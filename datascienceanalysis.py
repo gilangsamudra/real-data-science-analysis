3a6be8b971383ca369e4be3e98eee3e6e0f8b713
Gilang <gilang.samudrakuswana@gmail.com>
2020-04-26 00:07:25 +0700

    2

--- datascienceanalysis.py
+++ datascienceanalysis.py
@@ -1,6 +1,7 @@
 # import necessary libraries
 import pandas as pd
 import os
+import matplotlib.pyplot as plt
 
 # Merging 12 months of sales data into a singe file
 data1 = pd.read_csv('D:/Phyton Code/Contoh dari Github/Pandas-Data-Science-\
@@ -14,5 +15,29 @@ Pandas-Data-Science-Tasks-master/SalesAnalysis/Sales_Data')]
 for file in files:
     temp = pd.read_csv('D:/Phyton Code/Contoh dari Github/Pandas-Data-Science-\
 Tasks-master/SalesAnalysis/Sales_Data/' + file)
-
     all_data = pd.concat([all_data, temp])
+
+# Clean up Nan Values
+all_data.isnull().sum()
+all_data.dropna(axis=0, how='any', inplace=True)
+
+
+all_data = all_data[all_data['Order Date'].str[0:2] != 'Or']
+
+# Add an additional column
+all_data['Month'] = all_data['Order Date'].str[0:2]
+all_data['Month'] = all_data['Month'].astype('int32')
+
+all_data['Quantity Ordered'] = pd.to_numeric(all_data['Quantity Ordered'])
+all_data['Price Each'] = pd.to_numeric(all_data['Price Each'])
+
+all_data['Sales'] = all_data['Quantity Ordered'] * all_data['Price Each']
+
+# max sales
+sales = all_data.groupby('Month').sum()
+maxi = sales['Sales'].idxmax()
+
+# Plot
+ax = sales.plot.bar(y='Sales', rot=1, legend=False)
+ax.set_ylabel('Sales in USD $')
+
