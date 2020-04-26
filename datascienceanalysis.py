08fd2dbfdffb109c32684ee7e2cb11f81e19d473
Gilang <gilang.samudrakuswana@gmail.com>
2020-04-26 16:59:31 +0700

    4

--- datascienceanalysis.py
+++ datascienceanalysis.py
@@ -11,6 +11,8 @@ def get_state(address):
     return address.split(',')[2].split(' ')[1]
 
 
+# plt.style.use('fivethirtyeight')
+
 # Merging 12 months of sales data into a singe file
 data1 = pd.read_csv('D:/Phyton Code/Contoh dari Github/Pandas-Data-Science-\
 Tasks-master/SalesAnalysis/Sales_Data/Sales_April_2019.csv')
@@ -46,8 +48,10 @@ sales1 = all_data.groupby('Month').sum()
 maxi_month = sales1['Sales'].idxmax()
 
 # Plot
-ax1 = sales1.plot.bar(y='Sales', rot=1, legend=False)
+plt.figure(num=1, dpi=300)
+ax1 = sales1.plot.bar(y='Sales', grid=False, rot=0, legend=False, ax=plt.gca())
 ax1.set_ylabel('Sales in USD $')
+ax1.grid(b=True, which='major', axis='y', ls='--', lw=.5, c='k', alpha=.3)
 
 
 # extracting cities data
@@ -57,6 +61,9 @@ all_data['City'] = all_data['Purchase Address'].apply(lambda x: f"{get_city(x)} 
 sales2 = all_data.groupby('City').sum()
 maxi_city = sales2['Sales'].idxmax()
 
-ax2 = sales2.plot.bar(y='Sales', rot=1, legend=False)
+plt.figure(num=2, dpi=300)
+ax2 = sales2.plot.bar(y='Sales', grid=False, rot=90, legend=False, ax=plt.gca())
 ax2.set_ylabel('Sales in USD $')
+ax2.grid(b=True, which='major', axis='y', ls='--', lw=.5, c='k', alpha=.3)
+
 
