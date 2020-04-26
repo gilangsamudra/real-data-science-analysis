2cf51da16b03f95ab651f8b9e1f2645239280645
Gilang <gilang.samudrakuswana@gmail.com>
2020-04-26 14:34:35 +0700

    3

--- datascienceanalysis.py
+++ datascienceanalysis.py
@@ -3,6 +3,14 @@ import pandas as pd
 import os
 import matplotlib.pyplot as plt
 
+
+def get_city(address):
+    return address.split(',')[1]
+
+def get_state(address):
+    return address.split(',')[2].split(' ')[1]
+
+
 # Merging 12 months of sales data into a singe file
 data1 = pd.read_csv('D:/Phyton Code/Contoh dari Github/Pandas-Data-Science-\
 Tasks-master/SalesAnalysis/Sales_Data/Sales_April_2019.csv')
@@ -33,11 +41,22 @@ all_data['Price Each'] = pd.to_numeric(all_data['Price Each'])
 
 all_data['Sales'] = all_data['Quantity Ordered'] * all_data['Price Each']
 
-# max sales
-sales = all_data.groupby('Month').sum()
-maxi = sales['Sales'].idxmax()
+# max sales by Month
+sales1 = all_data.groupby('Month').sum()
+maxi_month = sales1['Sales'].idxmax()
 
 # Plot
-ax = sales.plot.bar(y='Sales', rot=1, legend=False)
-ax.set_ylabel('Sales in USD $')
+ax1 = sales1.plot.bar(y='Sales', rot=1, legend=False)
+ax1.set_ylabel('Sales in USD $')
+
+
+# extracting cities data
+all_data['City'] = all_data['Purchase Address'].apply(lambda x: f"{get_city(x)} ({get_state(x)})")
+
+# Max Sales by City
+sales2 = all_data.groupby('City').sum()
+maxi_city = sales2['Sales'].idxmax()
+
+ax2 = sales2.plot.bar(y='Sales', rot=1, legend=False)
+ax2.set_ylabel('Sales in USD $')
 
